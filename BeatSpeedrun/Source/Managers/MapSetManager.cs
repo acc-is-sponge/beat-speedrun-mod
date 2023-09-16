6646ffacb4928797dc3f0e5a7b918ca7cf253e46
acc-is-sponge <acc.is.sponge@gmail.com>
2023-09-16 17:14:57 +0900

    Change MapSetManager behavior
    - Use `custom:` prefix for custom mapsets
    - Don't cache MOD-provided mapsets

--- BeatSpeedrun/Source/Managers/MapSetManager.cs
+++ BeatSpeedrun/Source/Managers/MapSetManager.cs
@@ -15,86 +15,60 @@ namespace BeatSpeedrun.Managers
         private readonly ConcurrentDictionary<string, Task<MapSet>> _mapSets =
             new ConcurrentDictionary<string, Task<MapSet>>();
 
-        internal Task<MapSet> GetAsync(string uriOrRelativePath, CancellationToken ct = default)
+        internal Task<MapSet> GetAsync(string mapSetPath, CancellationToken ct = default)
         {
-            return _mapSets.GetOrAdd(uriOrRelativePath, LoadAsync).WithCancellation(ct);
+            return _mapSets
+                .AddOrUpdate(
+                    mapSetPath,
+                    path => LoadAsync(path),
+                    (path, t) => t.IsFaulted ? LoadAsync(path, TimeSpan.FromSeconds(3)) : t)
+                .WithCancellation(ct);
         }
 
-        private async Task<MapSet> LoadAsync(string uriOrRelativePath)
+        private async Task<MapSet> LoadAsync(string mapSetPath, TimeSpan delay = default)
         {
-            Uri uri;
+            await Task.Delay(delay);
 
             try
             {
-                uri = new Uri(uriOrRelativePath);
-            }
-            catch
-            {
-                uri = null;
-            }
-
-            if (uri != null && (uri.Scheme == "https" || uri.Scheme == "http"))
-            {
-                return await LoadFromWebAsync(uri);
-            }
-
-            var relativePath = uriOrRelativePath.Split('/');
-            if (relativePath.Any(part => part == ".."))
-            {
-                throw new ArgumentException("MapSet path cannot contain '..'");
-            }
-
-            try
-            {
-                return LoadFromCache(relativePath);
-            }
-            catch (Exception ex)
-            {
-                var isIgnorableError = ex is FileNotFoundException || ex is DirectoryNotFoundException;
-                if (!isIgnorableError)
+                if (mapSetPath.StartsWith("https:") || mapSetPath.StartsWith("http:"))
                 {
-                    Plugin.Log.Warn($"Could not load mapset '{string.Join("/", relativePath)}' from cache:\n{ex}");
+                    var uri = new Uri(mapSetPath);
+                    var json = await HttpClient.GetStringAsync(uri);
+                    return MapSet.FromJson(json);
+                }
+
+                if (mapSetPath.StartsWith("custom:"))
+                {
+                    var relativePath = mapSetPath.Substring("custom:".Length).Replace('\\', '/').Split('/');
+                    if (relativePath.Any(part => part == ".."))
+                    {
+                        throw new ArgumentException("Custom MapSet path cannot contain '..'");
+                    }
+                    var filePath = Path.Combine(CustomDirectory, Path.Combine(relativePath));
+                    var json = File.ReadAllText(filePath);
+                    return MapSet.FromJson(json);
+                }
+
+                {
+                    var uri = new Uri(ModProvidedBaseUri + mapSetPath);
+                    var json = await HttpClient.GetStringAsync(uri);
+                    return MapSet.FromJson(json);
                 }
             }
-
-            return await LoadFromRepositoryAsync(relativePath);
-        }
-
-        private async Task<MapSet> LoadFromWebAsync(Uri uri)
-        {
-            var json = await HttpClient.GetStringAsync(uri);
-            // We don't cache mapsets from the web
-            return MapSet.FromJson(json);
-        }
-
-        private MapSet LoadFromCache(string[] relativePath)
-        {
-            // Indeed, you can place your own mapsets in the cache directory
-            var cache = Path.Combine(CacheDirectory, Path.Combine(relativePath));
-            var json = File.ReadAllText(cache);
-            return MapSet.FromJson(json);
-        }
-
-        private async Task<MapSet> LoadFromRepositoryAsync(string[] relativePath)
-        {
-            var uri = new Uri(RepositoryRoot + string.Join("/", relativePath));
-            var json = await HttpClient.GetStringAsync(uri);
-            var mapSet = MapSet.FromJson(json);
-            try
+            catch (OperationCanceledException)
             {
-                var cache = Path.Combine(CacheDirectory, Path.Combine(relativePath));
-                Directory.CreateDirectory(Path.GetDirectoryName(cache));
-                File.WriteAllText(cache, json);
+                throw;
             }
             catch (Exception ex)
             {
-                Plugin.Log.Warn($"Failed to cache mapset '{string.Join("/", relativePath)}':\n{ex}");
+                Plugin.Log.Warn($"Could not load mapset '{mapSetPath}':\n{ex}");
+                throw;
             }
-            return mapSet;
         }
 
-        private static readonly string CacheDirectory = Path.Combine(Environment.CurrentDirectory, "UserData", "BeatSpeedrun", "MapSets");
-        private static readonly string RepositoryRoot = "https://raw.githubusercontent.com/acc-is-sponge/beat-speedrun-mapsets/main/";
+        private static readonly string CustomDirectory = Path.Combine(Environment.CurrentDirectory, "UserData", "BeatSpeedrun", "CustomMapSets");
+        private static readonly string ModProvidedBaseUri = "https://raw.githubusercontent.com/acc-is-sponge/beat-speedrun-mapsets/main/";
         private static readonly HttpClient HttpClient = new HttpClient();
     }
 }
