7bb33825dcf2256bf320628e20cf87537cdcab94
rakkyo150 <86054813+rakkyo150@users.noreply.github.com>
2023-09-22 00:03:39 +0900

    Add map set star display

--- BeatSpeedrun/Source/Plugin.cs
+++ BeatSpeedrun/Source/Plugin.cs
@@ -4,6 +4,9 @@ using IPA.Config.Stores;
 using IPALogger = IPA.Logging.Logger;
 using SiraUtil.Zenject;
 using BeatSpeedrun.Installers;
+using System;
+using HarmonyLib;
+using System.Reflection;
 
 namespace BeatSpeedrun
 {
@@ -12,6 +15,8 @@ namespace BeatSpeedrun
     {
         internal static Plugin Instance { get; private set; }
         internal static IPALogger Log { get; private set; }
+        private const string HarmonyId = "com.github.acc-is-sponge.beat-speedrun-mod";
+        private static readonly Harmony harmony = new Harmony(HarmonyId);
 
         [Init]
         public void Init(IPALogger logger, Config conf, Zenjector zenjector)
@@ -23,5 +28,59 @@ namespace BeatSpeedrun
             zenjector.Install<MenuInstaller>(Location.Menu);
             Log.Info("BeatSpeedrun initialized.");
         }
+
+        /// <summary>
+        /// Called when the plugin is enabled (including when the game starts if the plugin is enabled).
+        /// </summary>
+        [OnEnable]
+        public void OnEnable()
+        {
+            ApplyHarmonyPatches();
+        }
+
+        /// <summary>
+        /// Called when the plugin is disabled and on Beat Saber quit. It is important to clean up any Harmony patches, GameObjects, and Monobehaviours here.
+        /// The game should be left in a state as if the plugin was never started.
+        /// Methods marked [OnDisable] must return void or Task.
+        /// </summary>
+        [OnDisable]
+        public void OnDisable()
+        {
+            RemoveHarmonyPatches();
+        }
+
+        #region Harmony
+        /// <summary>
+        /// Attempts to apply all the Harmony patches in this assembly.
+        /// </summary>
+        private void ApplyHarmonyPatches()
+        {
+            try
+            {
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                Plugin.Log?.Error("Error applying Harmony patches: " + ex.Message);
+                Plugin.Log?.Debug(ex);
+            }
+        }
+
+        /// <summary>
+        /// Attempts to remove all the Harmony patches that used our HarmonyId.
+        /// </summary>
+        private void RemoveHarmonyPatches()
+        {
+            try
+            {
+                harmony.UnpatchSelf();
+            }
+            catch (Exception ex)
+            {
+                Plugin.Log?.Error("Error removing Harmony patches: " + ex.Message);
+                Plugin.Log?.Debug(ex);
+            }
+        }
+        #endregion
     }
 }
---
+++ BeatSpeedrun/Source/Views/StarDisplay.cs
@@ -0,0 +1,104 @@
+﻿using BeatSaberMarkupLanguage;
+using BeatSpeedrun.Controllers;
+using BeatSpeedrun.Models;
+using BeatSpeedrun.Models.Speedrun;
+using BeatSpeedrun.Services;
+using BeatSpeedrun.Source.Harmony;
+using BeatSpeedrun.Views;
+using HMUI;
+using IPA.Utilities;
+using ModestTree;
+using System;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace BeatSpeedrun.Source.Views
+{
+    internal class StarDisplay : IInitializable, IDisposable
+    {
+        private GameObject extraUI = null;
+        private float? star = null;
+        private readonly SpeedrunFacilitator _speedrunFacilitator;
+
+        public StarDisplay(SpeedrunFacilitator speedrunFacilitator)
+        {
+            _speedrunFacilitator = speedrunFacilitator;
+        }
+
+        private void UpdateStarDisplay(StandardLevelDetailView standardLevelDetailView)
+        {            
+            star = GetStar(standardLevelDetailView);
+
+            if (star == null)
+            {
+                GameObject.Destroy(extraUI);
+                return;
+            }
+
+            if (extraUI == null)
+            {
+                // the localPostion of the LevelParamsPanel can change
+                LevelParamsPanel levelParamsPanel = FieldAccessor<StandardLevelDetailView, LevelParamsPanel>.Get(standardLevelDetailView, "_levelParamsPanel");
+                // the localPostion of the LevelBar would not change
+                LevelBar levelBar = FieldAccessor<StandardLevelDetailView, LevelBar>.Get(standardLevelDetailView, "_levelBar");
+                TextMeshProUGUI[] temp = levelParamsPanel.GetComponentsInChildren<TextMeshProUGUI>();
+                ImageView[] temp2 = levelBar.GetComponentsInChildren<ImageView>();
+                extraUI = GameObject.Instantiate(temp[0].transform.parent.gameObject, temp2[0].transform);
+                extraUI.transform.localPosition -= new Vector3(-8, 12f);
+            }
+
+            ModifyField();
+            ModifyHoverHint();
+        }
+
+        private void ModifyField()
+        {
+            CurvedTextMeshPro field = extraUI.GetComponentInChildren<CurvedTextMeshPro>();
+            ImageView imageView = field.transform.parent.Find("Icon").GetComponent<ImageView>();
+            GameObject.Destroy(imageView);
+            field.text = $"★{star.Value:F2}";
+            field.color = Color.white;
+        }
+
+        private void ModifyHoverHint()
+        {
+            HoverHint hoverHint = extraUI.GetComponentInChildren<HoverHint>();
+            GameObject.Destroy(extraUI.GetComponentInChildren<LocalizedHoverHint>());
+            HoverHintController hoverHintController = Resources.FindObjectsOfTypeAll<HoverHintController>().First();
+            hoverHint.SetField("_hoverHintController", hoverHintController);
+            hoverHint.text = "Speedrun Star!";
+        }
+
+        private float? GetStar(StandardLevelDetailView standardLevelDetailView)
+        {
+            Speedrun speedrun = _speedrunFacilitator.Current;
+            IBeatmapLevel beatmapLevel = standardLevelDetailView.selectedDifficultyBeatmap.level;
+            BeatmapDifficulty difficulty = standardLevelDetailView.selectedDifficultyBeatmap.difficulty;
+            IDifficultyBeatmapSet difficultyBeatmapSet = standardLevelDetailView.selectedDifficultyBeatmap.parentDifficultyBeatmapSet;
+            string[] levelInfo = beatmapLevel.levelID.Split('_');
+            string characteristic = difficultyBeatmapSet.beatmapCharacteristic.serializedName;
+            float? star = speedrun?.MapSet?.GetStar(levelInfo[2], new DifficultyRaw(difficulty, "Solo" + characteristic));
+            return star;
+        }
+
+        private void ShowStarWithoutArguments()
+        {
+            StandardLevelDetailView standardLevelDetailView = Resources.FindObjectsOfTypeAll<StandardLevelDetailView>().First();
+            UpdateStarDisplay(standardLevelDetailView);
+        }
+
+        public void Initialize()
+        {
+            StandardLevelDetailViewPatch.didRefreshContentEvent += UpdateStarDisplay;
+            _speedrunFacilitator.OnCurrentSpeedrunChanged += ShowStarWithoutArguments;
+        }
+
+        public void Dispose()
+        {
+            StandardLevelDetailViewPatch.didRefreshContentEvent -= UpdateStarDisplay;
+            _speedrunFacilitator.OnCurrentSpeedrunChanged -= ShowStarWithoutArguments;
+        }
+    }
+}
